9f6b9943a7631675e4d22f99f787f6f38196463e
20192931 <gowhidol2@gmail.com>
2023-01-24 03:24:18 +0900

    이분탐색

---
+++ 1920.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int,input().split()))
+m = int(input())
+check = list(map(int,input().split()))
+a.sort()
+for i in check:
+    start = 0
+    end = n - 1
+    ok=True
+    while start<=end:
+        mid = (start+end)//2
+        if i<a[mid]:
+            end = mid-1
+            continue
+        elif i>a[mid]:
+            start = mid+1
+        else:
+            print(1)
+            ok=False
+            break
+    if ok:
+        print(0)
+
