a3059e09aee6c45e9b23e07b9cbaa30940409856
20192931 <gowhidol2@gmail.com>
2023-02-04 21:44:15 +0900

    이분그래프 bfs 2/4

--- hard/1707.py
+++ hard/1707.py
@@ -3,29 +3,25 @@ from collections import deque
 input = sys.stdin.readline
 def bfs():
     v, e = map(int, input().split())
-    a = [[] for _ in range(v + 1)]
+    graph = [[] for _ in range(v + 1)]
     for o in range(e):
         x, y = map(int, input().split())
-        a[x].append(y)
-        a[y].append(x)
-    visited = [[False,True] for o in range(v+1)] # [방문여부, 색깔(빨강or파랑)]
+        graph[x].append(y)
+        graph[y].append(x)
+    visited = [False] * (v+1) # ***False => 방문 x / 1,-1 => 색깔 구분 (0=False,1=-1=True)
     q = deque([])
     for i in range(1,v+1): #모든 노드가 연결되어 있지않을 수도 있어서 모두 확인 ex) 1 (2,3,4) 5
-        if visited[i][0] == False:
+        if not visited[i]:
             q.append(i)
-            visited[i][0] = True
+            visited[i] = 1
         while q:
             x = q.popleft()
-            for k in a[x]:
-                if visited[k][0] == False: # bfs
+            for k in graph[x]:
+                if not visited[k]: # bfs
                     q.append(k)
-                    if visited[x][1]:
-                        visited[k] = [True, False]
-                    else:
-                        visited[k] = [True, True]
-                else: # *** 인접한 노드 중에 같은 색깔 있는지 확인 ***
-                    if visited[k][1] == visited[x][1]:
-                        return False
+                    visited[k] = visited[x] * -1
+                elif visited[k] == visited[x]: # *** 인접한 노드 중에 같은 색깔 있는지 확인 ***
+                    return False
     return True
 for _ in range(int(input())):
     if bfs():
