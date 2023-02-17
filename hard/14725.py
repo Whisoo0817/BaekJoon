c1045df1b5f1b0413fccb52718f591e1182aa75b
20192931 <gowhidol2@gmail.com>
2023-02-18 00:45:25 +0900

    개미굴: 참고한 깔끔한 풀이 2/18

--- hard/14725.py
+++ hard/14725.py
@@ -1,26 +1,18 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 feed = []
 for i in range(int(input())):
-    feed.append(list(input().strip().split()))
-    feed[i].remove(feed[i][0])
-feed.sort()
-new = []
-for i in range(len(feed)):
-    new.append(deque([]))
-    for j in range(len(feed[i])):
-         new[-1].append("--"*j + feed[i][j])
-
-while len(new[-1])>0:
-    total = 0
-    for i in range(len(new)):
-        if new[i] and total == 0:
-            tmp = new[i].popleft()
-            total = 1
-            print(tmp)
-        elif new[i] and total != 0:
-            if new[i][0] != tmp:
-                break
-            else:
-                new[i].popleft()
+    feed.append(input().strip().split()[1:])
+tmp = []
+for i in sorted(feed):
+    count = 0
+    for j in range(len(tmp)):
+        if tmp[j] == i[j]:
+            count += 1
+        else:
+            break
+    cnt = count
+    for j in range(count, len(i)):
+        print('--'*cnt + i[j])
+        cnt += 1
+    tmp = i
